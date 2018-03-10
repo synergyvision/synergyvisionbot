7bee1909658e1fb98ff6e3abcf99f5065e9f9119
Alexander A. Ramírez M <alexander.ramirez@synergy-gb.com>
2018-03-10 10:48:01 -0400

    First bot

---
+++ app.js
@@ -0,0 +1,26 @@
+const Telegraf = require('telefraf')
+
+const bot = new Telegraf(process.env.BOT_TOKEN)
+
+bot.start((context)=>{
+  console.log('synergyvisionbot started', context.from.id)
+  return context.reply('El Bot de Synergy Vision te da la bienvenida.')
+})
+
+bot.command('ayuda', (context)=> {
+  context.reply('Esta es la ayuda!!')
+})
+
+bot.hears('hola', (context)=>{
+  context.reply('Hola cómo estas?')
+})
+
+bot.hears(/compra/curso, (context)=> {
+  context.reply('Compra! Compra!')
+})
+
+bot.on('sticker', (context)=>{
+  context.reply('👍')
+})
+
+bot.startPolling()
